59a57d8bef295e8264870cdb55bb176897363d20
Lukasz S <ukasz668@gmail.com>
2023-03-06 15:32:09 +0100

    Config file

---
+++ config.py
@@ -0,0 +1,14 @@
+
+# Post language
+language = "polish"
+
+# Temp images folder
+path = "images"
+
+# WordPress
+DOMAIN = ""
+AUTH_USER = 'Admin'
+AUTH_PASSWORD = ''
+
+# OpenAi API KEY
+API_KEY = ""
