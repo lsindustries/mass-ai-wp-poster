eb0e56742b29c0374fae7c0070acf5287fb54af1
Lukasz S <ukasz668@gmail.com>
2023-03-06 18:30:12 +0100

    finall

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import sleep
 
 
 img_dir_cleaner()
-keyword = input('Keyword for content creator (example: \'pozycjonowanie stron\'): ')
+keyword = input('Keyword for content creator (example: \'seo\'): ')
 titles = topic_list(keyword)
 
 q_titles = len(titles)
